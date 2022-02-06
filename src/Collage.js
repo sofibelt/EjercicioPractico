193b36026aa0aa616d07f8b83b6cd82a4d446c5d
sofibelt <anasobeltran@gmail.com>
2022-02-06 11:49:06 -0500

    se logro el cambio de imagen a pensa se pasa el mouse por encima

--- src/Collage.js
+++ src/Collage.js
@@ -1,13 +1,28 @@
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
+
 function Collage() {
+
+  const [seMuestra, cambiarSeMuestra] = useState(false);
+
   return (
     <div>
       <div id="PrimeraImagenNuevaColeccion" class="row">
         <div class="col-7 ">
           <div  class="row">
               <div class="col-3 ">
-                <img  src={require('./img/paralimpicos.jpeg')}  id="PrimeraImagenCollage" class="EfectoPrimeraImagen"/>
+                <button
+                  onMouseEnter={() => cambiarSeMuestra(true)}
+                  onMouseLeave={() => cambiarSeMuestra(false)}>
+                  {seMuestra && (
+                    <img  src={require('./img/paralimpicos.jpeg')}  id="PrimeraImagenCollage" class="EfectoPrimeraImagen"/>
+                  )}
+                  {!seMuestra && (
+                    <img  src={require('./img/silladeruedas.jpeg')}  id="PrimeraImagenCollage" class="EfectoPrimeraImagen"/>
+                  )}
+                </button>
+
               </div>
               <div  class="col-2 ">
                 <img  src={require('./img/paralimpicos.jpeg')}  id="SegundaImagenCollage" />
