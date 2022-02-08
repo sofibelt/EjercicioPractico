7a4d481d8b35f352c51bbf8dd57bf290979cec31
sofibelt <anasobeltran@gmail.com>
2022-02-07 21:17:43 -0500

    cambio de opciones, cuando se presiona un boton

--- src/Opciones.js
+++ src/Opciones.js
@@ -1,19 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import React, { useState } from 'react';
 import NuevaColeccion from './NuevaColeccion';
+import OpcionesProductos from './OpcionesProductos';
+
+
 
 function Opciones() {
+
+  const [primeraOpcion, cambiaPrimeraOpcion] = useState(true);
+  const [segundaOpcion, cambiaSegundaOpcion] = useState(false);
+  const [terceraOpcion, cambiaTerceraOpcion] = useState(false);
 
   return (
     <div id="Opciones">
       <div class="btn-group offset-3" role="group" aria-label="Basic example">
-        <button id="BotonesOpciones" type="button" class="btn btn-primary">SILLA DE RUEDAS</button>
-        <button id="BotonesOpciones" type="button" class="btn btn-primary">TERAPIA EN EL HOGAR</button>
-        <button id="BotonesOpciones" type="button" class="btn btn-primary">MOVILIDAD</button>
+        <button id="BotonesOpciones" type="button" class="btn btn-primary"  onClick={() => (cambiaPrimeraOpcion(true), cambiaSegundaOpcion(false), cambiaTerceraOpcion(false))}>SILLA DE RUEDAS</button>
+        <button id="BotonesOpciones" type="button" class="btn btn-primary"  onClick={() => (cambiaSegundaOpcion(true), cambiaPrimeraOpcion(false), cambiaTerceraOpcion(false))}>TERAPIA EN EL HOGAR</button>
+        <button id="BotonesOpciones" type="button" class="btn btn-primary"  onClick={() => (cambiaTerceraOpcion(true), cambiaSegundaOpcion(false), cambiaPrimeraOpcion(false))}>MOVILIDAD</button>
       </div>
       <div id="PadreOpciones">
           <img id="FondoOpciones" src={require('./img/fondosilladeruedas.jpeg')} />
           <div class="TextoOpciones">
-            <NuevaColeccion  titulo="SILLAS DE RUEDAS"/>
+            {primeraOpcion && (
+              <OpcionesProductos  titulo="SILLAS DE RUEDAS"/>
+            )}
+            {segundaOpcion && (
+              <OpcionesProductos  titulo="TERAPIA EN EL HOGAR"/>
+            )}
+            {terceraOpcion && (
+              <OpcionesProductos  titulo="MOVILIDAD"/>
+            )}
+
           </div>
       </div>
     </div>
---
+++ src/OpcionesProductos.js
@@ -0,0 +1,70 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React, { useState } from 'react';
+import Producto from './Producto'
+import PrecioProducto from './PrecioProducto';
+
+function NuevaColeccion(props) {
+
+  const [seMuestraPrimeraImagen, cambiarSeMuestraPrimeraImagen] = useState(false);
+  const [seMuestraSegundaImagen, cambiarSeMuestraSegundaImagen] = useState(false);
+  const [seMuestraTerceraImagen, cambiarseMuestraTerceraImagen] = useState(false);
+  const [seMuestraCuartaImagen, cambiarseMuestraCuartaImagen] = useState(false);
+
+  return (
+    <div id="NuevaColeccion">
+      <h2 id="TextoOpcionesProductos" class="text-center">{props.titulo}</h2>
+      <div id="PrimeraImagenNuevaColeccion"  className="row ">
+            <div id="ImagenesNuevaColleccion" class="form-group  "  >
+              <button
+                onMouseEnter={() => cambiarSeMuestraPrimeraImagen(true)}
+                onMouseLeave={() => cambiarSeMuestraPrimeraImagen(false)}>
+                {seMuestraPrimeraImagen && (
+                  <PrecioProducto />
+                )}
+                {!seMuestraPrimeraImagen && (
+                  <Producto/>
+                )}
+              </button>
+            </div>
+            <div id="ImagenesNuevaColleccion" class="form-group">
+              <button
+                onMouseEnter={() => cambiarSeMuestraSegundaImagen(true)}
+                onMouseLeave={() => cambiarSeMuestraSegundaImagen(false)}>
+                {seMuestraSegundaImagen && (
+                  <PrecioProducto />
+                )}
+                {!seMuestraSegundaImagen && (
+                  <Producto/>
+                )}
+              </button>
+            </div>
+            <div id="ImagenesNuevaColleccion" class="form-group" >
+              <button
+                onMouseEnter={() => cambiarseMuestraTerceraImagen(true)}
+                onMouseLeave={() => cambiarseMuestraTerceraImagen(false)}>
+                {seMuestraTerceraImagen && (
+                  <PrecioProducto />
+                )}
+                {!seMuestraTerceraImagen && (
+                  <Producto/>
+                )}
+              </button>
+            </div>
+            <div id="ImagenesNuevaColleccion" class="form-group  ">
+              <button
+                onMouseEnter={() => cambiarseMuestraCuartaImagen(true)}
+                onMouseLeave={() => cambiarseMuestraCuartaImagen(false)}>
+                {seMuestraCuartaImagen && (
+                  <PrecioProducto />
+                )}
+                {!seMuestraCuartaImagen && (
+                  <Producto/>
+                )}
+              </button>
+            </div>
+      </div>
+    </div>
+  );
+}
+
+export default NuevaColeccion;
